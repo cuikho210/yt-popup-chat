7f6862150b0a03fd58ab0a9bec740aaed261e8f6
cuikho210 <tonykms210@gmail.com>
2025-05-07 16:17:24 +0700

    integrate youtube_chat for live chat display

--- src/app.rs
+++ src/app.rs
@@ -1,35 +1,107 @@
-use iced::Center;
-use iced::widget::{Column, button, column, text};
+use anyhow::Result;
+use chrono::{DateTime, Utc};
+use iced::widget::text::Shaping;
+use iced::widget::{column, row, text};
+use iced::{Element, Task};
+use serde::{Deserialize, Serialize};
+use std::sync::{Arc, RwLock};
+use std::time::Duration;
+use tokio::sync::Mutex;
+use youtube_chat::item::MessageItem;
+use youtube_chat::live_chat::{Empty, LiveChatClient, LiveChatClientBuilder};
 
-#[derive(Debug, Clone, Copy)]
-pub enum AppMessage {
-    Increment,
-    Decrement,
+#[derive(Clone, Serialize, Deserialize, Debug)]
+pub struct ChatMessage {
+    pub id: String,
+    pub author: String,
+    pub message: String,
+    pub timestamp: Option<DateTime<Utc>>,
 }
 
-#[derive(Default)]
+#[derive(Debug, Clone)]
+pub enum AppMessage {
+    Tick,
+}
+
 pub struct App {
-    value: i64,
+    messages: Arc<RwLock<Vec<ChatMessage>>>,
+    client: Arc<Mutex<LiveChatClient<Empty, Empty, Empty, Empty>>>,
 }
 impl App {
-    pub fn update(&mut self, message: AppMessage) {
-        match message {
-            AppMessage::Increment => {
-                self.value += 1;
-            }
-            AppMessage::Decrement => {
-                self.value -= 1;
-            }
+    pub async fn try_new(url: impl AsRef<str>) -> Result<Self> {
+        let messages = Arc::new(RwLock::new(Vec::<ChatMessage>::new()));
+        let client = Arc::new(tokio::sync::Mutex::new({
+            let mut client = LiveChatClientBuilder::new().url(url)?.build();
+            client.start().await?;
+            client
+        }));
+
+        Ok(Self { messages, client })
+    }
+
+    pub fn update(&mut self, msg: AppMessage) -> Task<AppMessage> {
+        let client = self.client.clone();
+        let messages = self.messages.clone();
+        match msg {
+            AppMessage::Tick => Task::future(async move {
+                let chats = {
+                    let mut guard = client.lock().await;
+                    guard.fetch().await.expect("Cannot fetch new chat")
+                };
+
+                let chats: Vec<ChatMessage> = chats
+                    .into_iter()
+                    .map(|chat| {
+                        let parts: Vec<String> = chat
+                            .message
+                            .into_iter()
+                            .map(|v| match v {
+                                MessageItem::Emoji(emoji) => {
+                                    emoji.emoji_text.unwrap_or(String::new())
+                                }
+                                MessageItem::Text(text) => text,
+                            })
+                            .collect();
+                        ChatMessage {
+                            id: chat.id,
+                            author: chat.author.name.unwrap_or("Unknow".to_string()),
+                            message: parts.join(" "),
+                            timestamp: chat.timestamp,
+                        }
+                    })
+                    .collect();
+
+                {
+                    let mut guard = messages.write().expect("Failed to acquire write lock");
+                    guard.extend(chats);
+                }
+
+                tokio::time::sleep(Duration::from_millis(500)).await;
+                AppMessage::Tick
+            }),
         }
     }
 
-    pub fn view(&self) -> Column<AppMessage> {
-        column![
-            button("Increment").on_press(AppMessage::Increment),
-            text(self.value).size(50),
-            button("Decrement").on_press(AppMessage::Decrement)
-        ]
-        .padding(20)
-        .align_x(Center)
+    pub fn view(&self) -> Element<AppMessage> {
+        let messages_elements: Vec<Element<_>> = if let Ok(messages) = self.messages.read() {
+            messages
+                .iter()
+                .map(|msg| {
+                    row![
+                        text(format!("[{}]", msg.author)).size(13),
+                        text(msg.message.clone())
+                            .size(13)
+                            .shaping(Shaping::Advanced),
+                    ]
+                    .spacing(16)
+                    .into()
+                })
+                .collect()
+        } else {
+            tracing::error!("Cannot read messages");
+            Vec::new()
+        };
+
+        column(messages_elements).into()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 mod app;
 
-use app::App;
-use iced::{Size, daemon::Appearance};
+use app::{App, AppMessage};
+use iced::{Font, Size, Task, daemon::Appearance, futures::executor::block_on};
 
 pub fn main() -> iced::Result {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        .with_file(true)
+        .with_line_number(true)
+        .init();
+
     iced::application("Popup Chat", App::update, App::view)
         .centered()
         .decorations(false)
@@ -14,7 +20,17 @@ pub fn main() -> iced::Result {
         })
         .style(|_state, _theme| Appearance {
             background_color: iced::Color::from_linear_rgba(0., 0., 0., 0.3),
-            text_color: Default::default(),
+            text_color: iced::Color::WHITE,
         })
-        .run()
+        .default_font(Font::with_name("Noto Sans"))
+        .run_with(|| {
+            let args: Vec<String> = std::env::args().collect();
+            let url = args
+                .get(1)
+                .expect("Please provide a YouTube video URL as the first command-line argument.");
+
+            let app = block_on(App::try_new(url)).expect("Failed to initialize App");
+            let task = Task::future(async { AppMessage::Tick });
+            (app, task)
+        })
 }
